474a800697c806f44ac91af06f2870399e237e5d
arghodayah <arghodayah@hotmail.com>
2024-05-10 18:44:48 +0300

    update stating run command to start

--- Dockerfiles/scripts/entrypoint.sh
+++ Dockerfiles/scripts/entrypoint.sh
@@ -10,7 +10,7 @@ echo "- Current Build Profile: $BUILD_PROFILE"
 cd $APP_PATH
 
 if [ "$BUILD_PROFILE" = "staging" ]; then
-npm run dev
+npm run start
 
 elif [ "$BUILD_PROFILE" = "production" ]; then
 npm run start
