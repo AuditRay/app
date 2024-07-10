0b13395830f6dc5b7e4d3e50e28afab7488633c1
Waleed Qadi <waleedqadi@Waleeds-MacBook-Pro-2.local>
2024-07-10 16:27:27 +0300

    added loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import {Box} from "@mui/material";
+import * as React from "react";
+
+export default function Loading() {
+    return (
+        <Box sx={{textAlign:"center", width:"100%", mt: 20}}>Please wait loading data...</Box>
+    )
+}
