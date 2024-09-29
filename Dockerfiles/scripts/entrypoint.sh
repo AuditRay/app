976b92c50f004f11871ce5d3eb86ff82ab93018d
Abdulrahman Ghodayah <39015074+arghodayah@users.noreply.github.com>
2024-09-29 18:34:48 +0300

    Update entrypoint.sh

--- Dockerfiles/scripts/entrypoint.sh
+++ Dockerfiles/scripts/entrypoint.sh
@@ -9,7 +9,10 @@ echo "- Current Build Profile: $BUILD_PROFILE"
 
 cd $APP_PATH
 
-if [ "$BUILD_PROFILE" = "staging" ]; then
+if [ "$BUILD_PROFILE" = "development" ]; then
+npm run start
+
+elif [ "$BUILD_PROFILE" = "staging" ]; then
 npm run start
 
 elif [ "$BUILD_PROFILE" = "production" ]; then
